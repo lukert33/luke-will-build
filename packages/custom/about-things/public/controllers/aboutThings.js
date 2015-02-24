8abd1bdc70b9ae4c3364eb8bfdcc96f47cc6801f
Luke Thomas <lukert33@gmail.com>
2015-02-24 14:39:19 -0500

    Add params and location to aboutThings ng controller.

--- packages/custom/about-things/public/controllers/aboutThings.js
+++ packages/custom/about-things/public/controllers/aboutThings.js
@@ -1,8 +1,8 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.about-things').controller('AboutThingsController', ['$scope', 'Global', 'AboutThings',
-  function($scope, Global, AboutThings) {
+angular.module('mean.about-things').controller('AboutThingsController', ['$scope', '$stateParams', '$location', 'Global', 'AboutThings',
+  function($scope, $stateParams, $location, Global, AboutThings) {
     $scope.global = Global;
     $scope.package = {
       name: 'about-things'
---
+++ packages/custom/about-things/server/controllers/aboutThings.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Article = mongoose.model('Article'),
+  _ = require('lodash');
